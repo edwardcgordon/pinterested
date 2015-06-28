33c922e3515332f28a8518f709e2ea88c655a2ee
Ed Gordon <edward.c.gordon@gmail.com>
2015-06-28 20:31:47 +0000

    devise config #1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = {host: 'edward.c.gordon.herokuapp.com' }
 end
